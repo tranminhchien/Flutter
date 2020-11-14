8da463b8f3b0088156c66e7bec122417bdf7d4a7
Vo Thanh <>
2020-11-14 15:23:19 +0700

    done bottom nav

--- lib/screen/home1_screen/home1_screen.dart
+++ lib/screen/home1_screen/home1_screen.dart
@@ -2,14 +2,148 @@ import 'package:flutter/material.dart';
 
 import 'home_cell/home1_cell_widget.dart';
 
-class Home1Screen extends StatelessWidget {
+class BottomItemModal {
+  String imgAsset;
+  String imgActiveAsset;
+  BottomItemModal({
+    this.imgActiveAsset,
+    this.imgAsset,
+  });
+}
+
+class Home1Screen extends StatefulWidget {
+  @override
+  _Home1ScreenState createState() => _Home1ScreenState();
+}
+
+class _Home1ScreenState extends State<Home1Screen> {
+  PageController _pageController = PageController();
+  List<BottomItemModal> listBottomItem = [
+    BottomItemModal(
+      imgActiveAsset: "assets/images/iconBottom2Active.png",
+      imgAsset: "assets/images/Bottom1.png",
+    ),
+    BottomItemModal(
+      imgActiveAsset: "assets/images/iconBottom2Active.png",
+      imgAsset: "assets/images/Bottom1.png",
+    ),
+    BottomItemModal(
+      imgActiveAsset: "assets/images/iconBottom2Active.png",
+      imgAsset: "assets/images/Bottom1.png",
+    ),
+    BottomItemModal(
+      imgActiveAsset: "assets/images/iconBottom2Active.png",
+      imgAsset: "assets/images/Bottom1.png",
+    ),
+    BottomItemModal(
+      imgActiveAsset: "assets/images/iconBottom2Active.png",
+      imgAsset: "assets/images/Bottom1.png",
+    ),
+  ];
+  int _selectedIndex = 0;
+  Widget bottomBarCell(BottomItemModal data, int index) {
+    return Container(
+      width: 30,
+      height: 50,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AnimatedOpacity(
+            opacity: _selectedIndex == index ? 1 : 0,
+            duration: Duration(milliseconds: 250),
+            child: ClipOval(
+              child: Container(
+                width: 10,
+                height: 10,
+                color: Colors.redAccent,
+              ),
+            ),
+          ),
+          Image.asset(
+            _selectedIndex == index ? data.imgActiveAsset : data.imgAsset,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget customeBottomBarWidget({ValueChanged<int> onTap}) {
+    return SizedBox(
+      height: 78,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(
+          listBottomItem.length,
+          (index) => GestureDetector(
+            onTap: ()=>onTap(index),
+            child: bottomBarCell(listBottomItem[index], index),
+          ),
+        ),
+      ),
+      // Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: [
+      //     GestureDetector(
+      //       onTap: (){
+      //         setState(() {
+      //           _selectedIndex = 0;
+      //         });
+      //         onTap(_selectedIndex);
+      //       },
+      //       child: Container(
+      //         width: 30,
+      //         height:_selectedIndex == 0 ? 50: 30,
+      //         color: Colors.white,
+      //         alignment: Alignment.bottomCenter,
+      //         child:Image.asset(
+      //           _selectedIndex == 0 ? "assets/images/iconBottom1Active.png":"assets/images/Bottom1.png",
+      //         ),
+      //       ),
+      //     ),
+      //     GestureDetector(
+      //       onTap: () => onTap(1),
+      //       child: Container(
+      //         width: 30,
+      //         height: 30,
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //     GestureDetector(
+      //       onTap: () => onTap(2),
+      //       child: Container(
+      //         width: 30,
+      //         height: 30,
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //     GestureDetector(
+      //       onTap: () => onTap(3),
+      //       child: Container(
+      //         width: 30,
+      //         height: 30,
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //     GestureDetector(
+      //       onTap: () => onTap(4),
+      //       child: Container(
+      //         width: 30,
+      //         height: 30,
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Container(
-          //  margin: const EdgeInsets.only(right: 75),
           child: Image.asset(
             "assets/images/HomeTitle.png",
           ),
@@ -34,59 +168,114 @@ class Home1Screen extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: ListView.separated(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return Home1CellWidget();
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Container(
-              width: double.infinity,
-              height: 1,
-              color: Colors.white,
-            );
-          },
+        child: PageView(
+          controller: _pageController,
+          children: [
+            ListView.separated(
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return Home1CellWidget();
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return Container(
+                  width: double.infinity,
+                  height: 1,
+                  color: Colors.white,
+                );
+              },
+            ),
+            Container(
+              color: Colors.amber,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.yellow,
+            ),
+          ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/Bottom1.png'),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/Bottom2.png'),
-               color: Colors.grey,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/Bottom3.png'),
-              color: Colors.grey,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/Bottom4.png'),
-               color: Colors.grey,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(
-              AssetImage('assets/images/Bottom5.png'),
-               color: Colors.grey,
-            ),
-            label: '',
-          ),
-        ],
-        selectedItemColor: Colors.amber[800],
-      ),
+      bottomNavigationBar: customeBottomBarWidget(onTap: (v) {
+        setState(() {
+          _selectedIndex = v;
+        });
+        _pageController.animateToPage(v,
+            duration: Duration(milliseconds: 250), curve: Curves.easeInOut);
+      }),
+      // SizedBox(
+      //   height: 208,
+      //   child: BottomNavigationBar(
+      //     type: BottomNavigationBarType.shifting,
+      //     showSelectedLabels: false,
+      //     onTap: (v){
+      //       setState(() {
+      //         _selectedIndex = v;
+      //       });
+      //       _pageController.animateToPage(v, duration: Duration(milliseconds:250), curve: Curves.easeInOut);
+      //     },
+      //     currentIndex:_selectedIndex ,
+      //     items: const <BottomNavigationBarItem>[
+      //       BottomNavigationBarItem(
+      //         icon: ImageIcon(
+      //           AssetImage('assets/images/Bottom1.png'),
+      //           color: Colors.grey,
+      //         ),
+      //         activeIcon:ImageIcon(
+      //           AssetImage('assets/images/iconBottom1Active.png'),
+      //           color: Colors.amber,
+      //         ) ,
+      //         label: 'Home',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: ImageIcon(
+      //           AssetImage('assets/images/Bottom2.png'),
+      //            color: Colors.grey,
+      //         ),
+      //          activeIcon:ImageIcon(
+      //           AssetImage('assets/images/iconBottom2Active.png'),
+      //           color: Colors.amber,
+      //         ) ,
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: ImageIcon(
+      //           AssetImage('assets/images/Bottom3.png'),
+      //           color: Colors.grey,
+      //         ),
+      //          activeIcon:ImageIcon(
+      //           AssetImage('assets/images/iconBottom1Active.png'),
+      //         ) ,
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: ImageIcon(
+      //           AssetImage('assets/images/Bottom4.png'),
+      //            color: Colors.grey,
+      //         ),
+      //          activeIcon:ImageIcon(
+      //           AssetImage('assets/images/iconBottom1Active.png'),
+      //         ) ,
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: ImageIcon(
+      //           AssetImage('assets/images/Bottom5.png'),
+      //            color: Colors.grey,
+      //         ),
+      //          activeIcon:ImageIcon(
+      //           AssetImage('assets/images/iconBottom1Active.png'),
+      //         ) ,
+      //         label: '',
+      //       ),
+      //     ],
+      //     // selectedItemColor: Colors.amber[800],
+      //     // unselectedItemColor: Colors.grey,
+      //   ),
+      // ),
     );
   }
 }
